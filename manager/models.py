71763513aba7289159acc16c4fb49557b7cd0b1c
gragragrao <mathbass.ken531@gmail.com>
2017-07-17 23:17:32 +0900

    modelの追加

--- manager/models.py
+++ manager/models.py
@@ -1,3 +1,72 @@
 from django.db import models
 
-# Create your models here.
+
+class Person(models.Model):
+
+    MAN = 0
+    WOMAN = 1
+
+    HOKKAIDO = 0
+    TOHOKU = 5
+    TOKYO = 10
+    CHIBA = 11
+    KANAGAWA = 12
+    SAITAMA = 13
+    TOCHIGI = 14
+    IBARAGI = 15
+    CHUBU = 20
+    KANSAI = 25
+    CHUGOKU = 30
+    SHIKOKU = 35
+    KYUSHU = 40
+    OKINAWA = 45
+
+    # 名前
+    name = CharField(max_length=128)
+    # 誕生日
+    birthday = models.DatetimeField()
+    # 性別
+    sex = models.IntegerField(editable=False)
+    # 出身地
+    address_from = models.IntegerField()
+    # 現住所
+    current_address = models.IntegerField()
+    # メールアドレス
+    email = models.EmailField()
+
+
+class Manager(models.Model):
+
+    # 部署の定数
+    DEP_ACCOUNTING = 0  # 経理
+    DEP_SALES = 5  # 営業
+    DEP_PRODUCTION = 10  # 製造
+    DEP_DEVELOPMENT = 15  # 開発
+    DEP_HR = 20  # 人事
+    DEP_FIN = 25  # 財務
+    DEP_AFFAIRS = 30  # 総務
+    DEP_PLANNING = 35  # 企画
+    DEP_BUSINESS = 40  # 業務
+    DEP_DISTR = 45  # 流通
+    DEP_IS = 50  # 情報システム
+
+    # 人
+    person = models.ForeignKey('Person')
+    # 部署
+    department = models.IntegerField()
+    # 着任時期
+    joined_at = models.DatetimeField()
+    # やめた時期
+    quited_at = models.DatetimeField(null=True, blank=True)
+
+
+class Worker(models.Model):
+
+    # 人
+    person
+    # 着任時期
+    joined_at = models.DatetimeField()
+    # やめた時期
+    quited_at = models.DatetimeField(null=True, blank=True)
+    # 担当上司
+    manager = models.ForeignKey('Manager')
