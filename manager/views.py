0ea1092b0ed69dcb76aab44ff09612d6406c0845
gragragrao <mathbass.ken531@gmail.com>
2017-08-11 17:17:15 +0900

    modelなど完成

--- manager/views.py
+++ manager/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import TemplateView
 from django.contrib.auth.views import login
+from django.contrib.auth import authenticate
 
 from manager.models import *
 
@@ -16,9 +17,9 @@ class CustomLoginView(TemplateView):
             return login(self.request, *args, **kwargs)
 
     def post(self, _, *args, **kwargs):
-        username = self.request.POST['username']
+        identifier = self.request.POST['identifier']
         password = self.request.POST['password']
-        user = authenticate(username=username, password=password)
+        user = authenticate(username=identifier, password=password)
         if user is not None and user.is_active:
             login(self.request, user)
             return redirect(self.get_next_redirect_url())
@@ -31,6 +32,31 @@ class CustomLoginView(TemplateView):
         if not redirect_url or redirect_url == '/':
             redirect_url = '/worker_list/'
         return redirect_url
+
+
+def person_registration(request, *args, **kwargs):
+
+    if request.POST:
+        form_data = request.POST
+
+        sex = Person.MAN if form_data['sex'] == 'male' else Person.WOMAN
+
+        person = Person(name=form_data['name'],
+                        birthday=form_data['birthday'],
+                        sex=sex,
+                        email=form_data['email'],
+                        address_from=form_data['address_from'],
+                        current_address=form_data['current_address'])
+
+        person.set_password(form_data['password'])
+        person.save()
+
+        # Activationを作成
+        activation = Activation(person=person)
+        activation.create_and_set_key()
+        activation.save()
+
+        return render(request, "registration_done.html", {"email": form_data['email']})
 
 
 class WorkerListView(TemplateView):
