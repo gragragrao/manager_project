96fecc107c28c5dfeaca06d6fe8c39324aabff5e
gragragrao <mathbass.ken531@gmail.com>
2017-11-03 22:04:27 +0900

    hijack完成

--- manager_project/urls.py
+++ manager_project/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     url(r'^login/', manager_view.CustomLoginView.as_view()),
     url(r'^logout/', manager_view.logout_view),
     url(r'^worker_list/', login_required(manager_view.WorkerListView.as_view())),
+    url(r'^hijack/', include('hijack.urls')),
 ]
 
 if settings.DEBUG:
